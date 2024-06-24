35cd1b3b11e2af2431094916097178c4d4405914
GitMarco27 <marco.sanguineti.info@gmail.com>
2024-06-24 15:57:50 +0200

    📈 new run_demo.sh

--- demo.py
+++ demo.py
@@ -5,6 +5,6 @@ from streamlit_post_message.streamlit_post_message import streamlit_post_message
 if __name__ == "__main__":
     st.set_page_config(layout="wide", page_title="Streamlit Post Message Demo")
 
-    payload = streamlit_post_message(sleep_time=1.0)
+    payload = streamlit_post_message(sleep_time=1.0, message_key="tool-init")
     with st.expander("Payload received from parent window: ", expanded=True):
         st.info(payload)
---
+++ run_demo.sh
@@ -0,0 +1,2 @@
+nohup streamlit run demo.py --server.port 8080 &
+python -m http.server 8000
